6876d42edbc19cdaf3b0ee96677bb507cd2c2919
ingridcriistine <ingridrochacristine@gmail.com>
2025-08-24 21:18:37 -0300

    add mock to table

--- manutencao-angular/src/app/components/tela-inicial-cliente/tela-inicial-cliente.ts
+++ manutencao-angular/src/app/components/tela-inicial-cliente/tela-inicial-cliente.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FilterPipe } from '../../pipes/filter-pipe';
+import { FormsModule } from '@angular/forms';
+import { MOCK_DATA_SOLICITACOES } from '../../models/mock-data-solicitacoes';
 
 @Component({
   selector: 'app-tela-inicial-cliente',
-  imports: [],
+  imports: [CommonModule, FilterPipe, FormsModule],
   templateUrl: './tela-inicial-cliente.html',
   styleUrl: './tela-inicial-cliente.css'
 })
-export class TelaInicialCliente {
+export class TelaInicialCliente implements OnInit {
+  solicitacoes = MOCK_DATA_SOLICITACOES;
+  searchText: string = '';
+  dateField?: string = '';
+  selectedDate?: Date;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+      this.http.get<any[]>('models/mock-data-solicitacoes.json').subscribe(data => {
+        this.solicitacoes = data;
+      });
+  }
 }
