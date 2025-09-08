a15c04473236161c5c5dca19d193e90b9a4cb9f5
ingridcriistine <ingridrochacristine@gmail.com>
2025-09-08 17:12:39 -0300

    tela inicial funcionario

--- manutencao-angular/src/app/app.routes.ts
+++ manutencao-angular/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { TelaCadastro, TelaLogin } from './pages/index';
 import { TelaInicialCliente } from './components/tela-inicial-cliente/tela-inicial-cliente';
+import { TelaInicialFuncionario } from './components/tela-inicial-funcionario/tela-inicial-funcionario';
 import { NovaSolicitacao } from './components/nova-solicitacao/nova-solicitacao';
 import { VisualizarOrcamento } from './components/visualizar-orcamento/visualizar-orcamento';
 import { PagarServico } from './components/pagar-servico/pagar-servico';
@@ -10,6 +11,7 @@ export const routes: Routes = [
     { path: '', component: TelaLogin },
     { path: 'cadastro', component: TelaCadastro },
     { path: 'tela-inicial-cliente', component: TelaInicialCliente },
+    { path: 'tela-inicial-funcionario', component: TelaInicialFuncionario },
     { path: 'nova-solicitacao', component: NovaSolicitacao },
     { path: 'visualizar-orcamento/:id', component: VisualizarOrcamento},
     { path: 'visualizar-servico/:id', component: VisualizarServico}, 
