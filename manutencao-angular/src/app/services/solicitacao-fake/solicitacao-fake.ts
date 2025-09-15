9dd99b75a675e88b2f13898003bf39fd4b79c8f7
Carolina Milano <carolinamilanorg@gmail.com>
2025-09-15 09:04:57 -0300

    Adição do service de SolicitaçãoFake para testes na página de detalhamento de solicitação - visão funcionário

---
+++ manutencao-angular/src/app/services/solicitacao-fake/solicitacao-fake.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { SolicitacaoDetalhe } from '../../models';
+
+const LS_SOLICITACOES = 'solicitacoes';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SolicitacaoFakeService {
+  private id = 0;
+  
+  listarTodos(): SolicitacaoDetalhe[] {
+    const solicitacoes = localStorage[LS_SOLICITACOES];
+
+    return solicitacoes ? JSON.parse(solicitacoes) : [];
+  }
+
+  inserir(solicitacao: SolicitacaoDetalhe): void {
+    const solicitacoes = this.listarTodos();
+
+    if (solicitacoes.length > 0) {
+      this.id = Math.max(...solicitacoes.map(solicitacao => solicitacao.idSolicitacao || 0)) + 1;
+    }
+    
+    solicitacao.idSolicitacao = this.id++;
+    
+    solicitacoes.push(solicitacao);
+
+    localStorage[LS_SOLICITACOES] = JSON.stringify(solicitacoes);
+  }
+
+  buscarPorId(id: number): SolicitacaoDetalhe {
+    const solicitacoes = this.listarTodos();
+
+    return solicitacoes.find(solicitacao => solicitacao.idSolicitacao === id)!;
+  }
+
+  atualizar(solicitacao: SolicitacaoDetalhe): void{
+    const solicitacoes = this.listarTodos();
+
+    solicitacoes.forEach((obj, index, lista) => {
+      if(solicitacao.idSolicitacao === obj.idSolicitacao) {
+        lista[index] = solicitacao;
+      }
+    });
+
+    localStorage[LS_SOLICITACOES] = JSON.stringify(solicitacoes);
+  }
+
+  remover(id: number): void {
+  }
+}
