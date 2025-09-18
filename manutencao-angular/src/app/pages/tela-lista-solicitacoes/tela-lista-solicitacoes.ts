0ee06086e5c734080c7102a5cbacf432ce56596d
ingridcriistine <ingridrochacristine@gmail.com>
2025-09-18 19:21:41 -0300

    new component

---
+++ manutencao-angular/src/app/pages/tela-lista-solicitacoes/tela-lista-solicitacoes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-lista-solicitacoes',
+  imports: [],
+  templateUrl: './tela-lista-solicitacoes.html',
+  styleUrl: './tela-lista-solicitacoes.css'
+})
+export class TelaListaSolicitacoes {
+
+}
