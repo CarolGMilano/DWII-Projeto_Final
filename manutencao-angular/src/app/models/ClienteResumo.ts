fe679410dd7d7a693d906cb10b70f70a2dba6e75
Carolina Milano <carolinamilanorg@gmail.com>
2025-10-05 19:33:13 -0300

    Deleção de arquivos

--- manutencao-angular/src/app/models/ClienteResumo.ts
+++
@@ -1,9 +0,0 @@
-import { Endereco } from './Endereco';
-
-export interface ClienteResumo {
-  idCliente?: number; 
-  nome: string;
-  cpf: string;
-  telefone: string;
-  endereco: Endereco; 
-}
--- manutencao-angular/src/app/models/EnumCepStatus.ts
+++
@@ -1,7 +0,0 @@
-export enum CepStatus {
-  Vazio = 'vazio',
-  Incompleto = 'incompleto',
-  Invalido = 'invalido',
-  Valido = 'valido',
-  ProblemaAPI = 'problemaAPI'
-}
