7f517d8d009ec0d7befc7634baea5b4c1a80e94d
ingridcriistine <ingridrochacristine@gmail.com>
2025-10-06 20:17:54 -0300

    arrumando get lista

--- manutencao-angular/src/app/pages/tela-lista-solicitacoes/tela-lista-solicitacoes.ts
+++ manutencao-angular/src/app/pages/tela-lista-solicitacoes/tela-lista-solicitacoes.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MOCK_DATA_SOLICITACOES } from '../../models/mock-data-solicitacoes';
 import { MatIconModule } from '@angular/material/icon';
+import { SolicitacaoService } from '../../services';
 
 @Component({
   selector: 'app-tela-lista-solicitacoes',
@@ -14,7 +15,8 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './tela-lista-solicitacoes.css'
 })
 export class TelaListaSolicitacoes implements OnInit {
-  solicitacoes = MOCK_DATA_SOLICITACOES;
+  // solicitacoes = MOCK_DATA_SOLICITACOES;
+  solicitacoes: any[] = [];
   searchText: string = '';
   dateField?: string = '';
   selectedDate?: Date | string;
@@ -22,12 +24,29 @@ export class TelaListaSolicitacoes implements OnInit {
   startDate?: Date;
   endDate?: Date;
 
-  constructor(private http: HttpClient) {}
+  // constructor(private http: HttpClient) { }
+
+  // ngOnInit(): void {
+  //   this.http.get<any[]>('models/mock-data-solicitacoes.json').subscribe(data => {
+  //     this.solicitacoes = data;
+  //   });
+  // }
+
+  constructor(private solicitacaoService: SolicitacaoService) { }
 
   ngOnInit(): void {
-      this.http.get<any[]>('models/mock-data-solicitacoes.json').subscribe(data => {
+    this.carregarSolicitacoes();
+  }
+
+  carregarSolicitacoes() {
+    this.solicitacaoService.getSolicitacoes().subscribe({
+      next: (data) => {
         this.solicitacoes = data;
-      });
+      },
+      error: (err) => {
+        console.error('Erro ao carregar solicitações', err);
+      }
+    });
   }
 
   filtrarHoje() {
@@ -36,7 +55,7 @@ export class TelaListaSolicitacoes implements OnInit {
     const mm = String(hoje.getMonth() + 1).padStart(2, '0');
     const dd = String(hoje.getDate()).padStart(2, '0');
 
-    this.selectedDate = `${yyyy}-${mm}-${dd}`; 
+    this.selectedDate = `${yyyy}-${mm}-${dd}`;
     this.startDate = undefined;
     this.endDate = undefined;
   }
