bd545b0110485806826a73cd6d847652a0d0b59c
Carolina Milano <carolinamilanorg@gmail.com>
2025-10-20 20:43:43 -0300

    Adição do Spring Boot

---
+++ manutencao-spring/manutencao/src/test/java/br/net/dwii/projeto/manutencao/ManutencaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.dwii.projeto.manutencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManutencaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
