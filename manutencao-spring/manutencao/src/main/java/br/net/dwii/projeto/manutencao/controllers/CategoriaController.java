61cc2299f66eb8d47b8ff1ccfd746ab28eaa95f2
milena579 <milecalegari@gmail.com>
2025-11-07 19:16:19 -0300

    controller categoria

--- manutencao-spring/manutencao/src/main/java/br/net/dwii/projeto/manutencao/controllers/CategoriaController.java
+++ manutencao-spring/manutencao/src/main/java/br/net/dwii/projeto/manutencao/controllers/CategoriaController.java
@@ -1,5 +1,29 @@
 package br.net.dwii.projeto.manutencao.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.net.dwii.projeto.manutencao.model.Categoria;
+
 public class CategoriaController {
-    
+    public static List<Categoria> categorias = new ArrayList<>();
+
+    @GetMapping("/categorias")
+    public ResponseEntity<List<Categoria>> listarCategorias() {
+        return ResponseEntity.ok(categorias);
+    }
+
+    @GetMapping("/categoria/{id}")
+    public ResponseEntity<Categoria> obterCategoriaPorId(@PathVariable("id") int id) {
+      Categoria c = categorias.stream()
+        .filter(categoria -> categoria.getId() == id).findAny()
+        .orElse(null);
+
+        if(c == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
 }
