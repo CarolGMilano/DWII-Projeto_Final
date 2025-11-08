d6b801f027578946d4ae53f2810f6495aabb8080
Carolina Milano <carolinamilanorg@gmail.com>
2025-11-07 21:10:28 -0300

    Atualização dos services para comunicação com backend.

--- manutencao-angular/src/app/services/cliente/cliente.ts
+++ manutencao-angular/src/app/services/cliente/cliente.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
 
 import { Cliente } from '../../shared';
 
@@ -11,23 +11,86 @@ import { Cliente } from '../../shared';
 export class ClienteService {
   private readonly _httpClient = inject(HttpClient)
   
-  BASE_URL = "http://localhost:3000/cliente";
+  BASE_URL = "http://localhost:8080/clientes";
 
   httpOptions = {
+    observe: "response" as "response",
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
     })
   }
-
-  listarTodos(): Observable<Cliente[]> {
-    return this._httpClient.get<Cliente[]>(this.BASE_URL, this.httpOptions);
+/*
+  listarTodos(): Observable<Cliente[] | null> {
+    return this._httpClient.get<Cliente[]>(this.BASE_URL, this.httpOptions).pipe(
+      map((resposta: HttpResponse<Cliente[]>) => {
+        if(resposta.status == 200){
+          return resposta.body;
+        } else {
+          return [];
+        }
+      }), 
+      catchError((erro, caught) => {
+        if(erro.status == 404) {
+          return of([]);
+        } else {
+          return throwError(() => erro);
+        }
+      })
+    );
+  }
+*/
+  inserir(cliente: Cliente): Observable<Cliente | null> {
+    return this._httpClient.post<Cliente>(
+      this.BASE_URL, 
+      JSON.stringify(cliente), 
+      this.httpOptions
+    ).pipe(
+      map((resposta: HttpResponse<Cliente>) => {
+        if(resposta.status == 201){
+          return resposta.body;
+        } else {
+          return null;
+        }
+      }), 
+      catchError((erro) => {
+        return throwError(() => erro);
+      })
+    );
   }
 
-  inserir(cliente: Cliente): Observable<Cliente> {
-    return this._httpClient.post<Cliente>(this.BASE_URL, JSON.stringify(cliente), this.httpOptions);
+  buscarPorId(id: number): Observable<Cliente | null> {
+    return this._httpClient.get<Cliente>(
+      `${this.BASE_URL}/${id}`, 
+      this.httpOptions
+    ).pipe(
+      map((resposta: HttpResponse<Cliente>) => {
+        if(resposta.status == 200){
+          return resposta.body;
+        } else {
+          return null;
+        }
+      }),
+      catchError((erro) => {
+        return throwError(() => erro);
+      })
+    );   
   }
 
-  buscarPorId(id: number): Observable<Cliente | undefined> {
-    return this._httpClient.get<Cliente>(this.BASE_URL + "/" + id, this.httpOptions);   
+  buscarPorUsuario(id: number): Observable<Cliente | null> {
+    return this._httpClient.get<Cliente>(
+      `${this.BASE_URL}/por-usuario?id=${id}`, 
+      this.httpOptions
+    ).pipe(
+      map((resposta: HttpResponse<Cliente>) => {
+        if(resposta.status == 200){
+          return resposta.body;
+        } else {
+          return null;
+        }
+      }),
+      catchError((erro) => {
+        return throwError(() => erro);
+      })
+    );   
   }
 }
