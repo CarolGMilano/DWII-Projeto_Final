e5d93e8bb5b130dfd00a661ab41d848737c01f4b
Carolina Milano <carolinamilanorg@gmail.com>
2025-11-16 18:07:43 -0300

    Correção de formatação de moeda para BRL em PT-BR.

--- manutencao-angular/src/app/models/HistoricoSolicitacao.ts
+++
@@ -1,9 +0,0 @@
-import { EstadoSolicitacao } from "./EnumEstadoSolicitacao";
-
-export interface HistoricoSolicitacao {
-  idSolicitacao: number;
-  dataHora: Date;
-  estado: string;
-  funcionario: string;
-  observacao?: string;
-}
